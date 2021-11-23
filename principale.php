bdd263b08481172d2c1913a33759ff9f5c21d203
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2021-11-23 15:18:16 -0500

    Départ du TP

---
+++ principale.php
@@ -0,0 +1,14 @@
+<?php 
+    include("inclusions/nav.php");
+?>
+    <!-- carrousel -->
+    <main id="article1">
+        <article id="article2">
+            <article id="article3">
+
+            </article>
+        </article>
+    </main>
+    <footer></footer>
+</body>
+</html>
---
+++ serveur.bat
@@ -0,0 +1,1 @@
+D:\Logiciels\php8010\php -S 127.0.0.1:8081
