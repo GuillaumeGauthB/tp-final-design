6e47622cf4d850358a49d3e7666a6f302e5c015b
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2021-11-30 15:03:22 -0500

    Pour mettre sur mac

--- v4/js/caroussel.js
+++ v4/js/caroussel.js
@@ -1,42 +1,8 @@
-var slidePosition = 1;
-SlideShow(slidePosition);
-
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow(slidePosition += n);
-}
-
-//  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-}
-
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("boites");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slidePosition-1].style.display = "grid";
-}
-
 // var slidePosition = 1;
 // SlideShow(slidePosition);
-// var slides = document.getElementsByClassName("caroussel");
 
 // // forward/Back controls
 // function plusSlides(n) {
-//   if ((slidePosition += n) == 1){
-//     slides.style.transform = "translate(100vw)";
-//   }
-//   else if ((slidePosition += n) == 2){
-//     slides.style.transform = "translate(100vw)";
-//   }
-//   else{
-
-//   }
 //   SlideShow(slidePosition += n);
 // }
 
@@ -47,14 +13,51 @@ function SlideShow(n) {
 
 // function SlideShow(n) {
 //   var i;
-//   var slides = document.getElementsByClassName("caroussel");
-//   if (n == 1){
-//     slides.style.transform = "translate(100vw)";
+//   var slides = document.getElementsByClassName("boites");
+//   if (n > slides.length) {slidePosition = 1}
+//   if (n < 1) {slidePosition = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
 //   }
-//   else if (n == 2){
-
-//   }
-//   else{
-
-//   }
+//   slides[slidePosition-1].style.display = "grid";
 // }
+
+let posSlider = 0;
+// var slides = document.getElementsByClassName("caroussel");
+var slides = document.getElementById("caroussel");
+SlideShow(posSlider, slides);
+
+// forward/Back controls
+function plusSlides(n) {
+  posSlider += n;
+}
+
+//  images controls
+// function currentSlide(n) {
+  
+// }
+
+function SlideShow(pos, slide) {
+  var i;
+  if(pos = -1){
+    slide.style.transform = "translate(0, -100vw);";
+  }
+  else if(pos = 0){
+    slide.style.transform = "translate(0, 0);"
+  }
+  else if(pos = 1){
+    slide.style.transform = "translate(0, 100vw);"
+  }
+  else if (pos < -1){
+    pos = 1;
+  }
+  else{
+    pos = -1;
+  }
+  // if (n > slides.length) {slidePosition = 1}
+  // if (n < 1) {slidePosition = slides.length}
+  // for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none";
+  // }
+  // slides[slidePosition-1].style.display = "grid";
+}
